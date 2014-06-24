93e42d72c4a88a485b7bad4a12253f8f284b1426
macubergeek <macubergeek@comcast.net>
2014-06-24 19:48:11 -0400

    adding a few tools

--- gitlist.sh
+++ gitlist.sh
@@ -109,6 +109,7 @@ https://github.com/bastibl/gr-ieee802-11.git
 https://github.com/dcramer/chardet.git
 https://github.com/client9/sslassert.git
 https://github.com/toddsiegel/spscan.git
+https://github.com/EarToEarOak/RTLSDR-Scanner.git
 EOF`
 cd /opt
 for i in $a
