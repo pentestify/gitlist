8734c550571978555b99f87ec9c0dad7f9e0f8f3
macubergeek <macubergeek@comcast.net>
2014-07-06 09:53:03 -0400

    adding a few tools

--- svn.sh
+++ svn.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-svn checkout http://impacket.googlecode.com/svn/trunk/impacket
-svn checkout http://edgessh.googlecode.com/svn/trunk/ edgessh
-svn checkout svn://www.engineeringforfun.com/svn/durzosploit/trunk
-svn checkout http://host-extract.googlecode.com/svn/trunk/host-extract
-svn checkout http://subterfuge.googlecode.com/svn/trunk subterfuge
+svn co http://impacket.googlecode.com/svn/trunk/impacket
+svn co http://edgessh.googlecode.com/svn/trunk/ edgessh-read-only
+svn checkout http://host-extract.googlecode.com/svn/trunk/ host-extract-read-only
+svn checkout http://subterfuge.googlecode.com/svn/trunk/ subterfuge-read-only
